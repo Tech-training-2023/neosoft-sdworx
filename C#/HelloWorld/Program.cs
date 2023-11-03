ae4e84c109e6e7b2059c7f9c60bb94945ab18ecc
Pushpinder <pushpinder.kaur@revature.com>
2023-11-03 12:41:40 +0000

    value-reference types

--- C#/HelloWorld/Program.cs
+++ C#/HelloWorld/Program.cs
@@ -2,21 +2,29 @@
 global using static System.Console;
 //user defined namespace
 namespace HelloWorld; // file scope namespace
-  // Types - class is a type just like structs, enums
-    class Program
+                      // Types - class is a type just like structs, enums
+class Program
+{
+    // Type member
+    static void Main()// starting point of execution
     {
-        // Type member
-        static void Main()// starting point of execution
-        {
-            //program statment
-            /*
-             C# is case sensitive
-             statements ends with semi-colon
-             C# is type safe as well
-             */
-            WriteLine("Welcome to SDWorx training!");
-        }
+        //program statment
+        /*
+         C# is case sensitive
+         statements ends with semi-colon
+         C# is type safe as well
+         */
+        WriteLine("Welcome to SDWorx training!");
+
+        //int a;//declaration
+        //a = 35; // assigning value
+
+        string a = "35";
+
+        WriteLine(a);
+
     }
+}
 // this will not be allowed if you use file scoped namespace before
 /*namespace RestaurantReviews
 {
